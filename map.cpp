ff562e1bc27e7ca74133a440486e4a3b3a209ba3
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2022-03-16 20:05:33 +0530

    learnt map find function

--- map.cpp
+++ map.cpp
@@ -21,7 +21,10 @@ int main(){
     for (auto x:mp){
         cout << x.first <<" " <<x.second << endl;
     }
+    if(mp.find(18) == mp.end()) cout << "founded";
+    else cout << "not";
 
+    // find function me agar mughe koi key nhi milti to mp.end() ke equal ho jaati hai hence vo true return krti hai
     cout << endl;
     cout << endl;
     cout << endl;
@@ -35,6 +38,9 @@ int main(){
      for (auto x:map){
         cout << x.first.first <<" "<<x.first.second<< " " <<x.second << endl;
     }
+
+    if(map.find({1,2}) == map.end()) cout << "founded pair" << endl;
+    if(map.find({2,1}) == map.end()) cout << "not founded" << endl;
 
 
 
